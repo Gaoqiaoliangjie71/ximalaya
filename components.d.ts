ac67fac62fbe637a6d040821552a702dd62ae013
sunfz <m13089426619@163.com>
2024-02-25 11:10:31 +0800

    4

--- components.d.ts
+++ components.d.ts
@@ -13,7 +13,6 @@ declare module 'vue' {
     RouterView: typeof import('vue-router')['RouterView']
     VanButton: typeof import('vant/es')['Button']
     VanIcon: typeof import('vant/es')['Icon']
-    VanLoading: typeof import('vant/es')['Loading']
     VanSkeleton: typeof import('vant/es')['Skeleton']
     VanSkeletonImage: typeof import('vant/es')['SkeletonImage']
     VanSkeletonParagraph: typeof import('vant/es')['SkeletonParagraph']
