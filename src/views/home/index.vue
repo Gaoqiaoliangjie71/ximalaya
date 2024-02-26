9457956ad7483b069bbe0508944c2153bf77ebd6
yuanxuke <2577238231@qq.com>
2024-02-26 11:48:41 +0800

    'yxk02'

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -307,8 +307,6 @@ onMounted(() => {
       background-color: #fa2800;
     }
   }
-<<<<<<< HEAD
-
   .foot {
     width: 100%;
     height: 180px;
@@ -328,7 +326,5 @@ onMounted(() => {
       font-weight: 400;
     }
   }
-=======
->>>>>>> yxk
 }
 </style>
